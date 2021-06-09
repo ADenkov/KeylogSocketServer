6ab474792b3a509d5c8298f766b7efbc5f5cde75
i444076 <a.denkov@student.fontys.nl>
2021-06-09 16:39:57 +0200

    debugging

--- socket_server.py
+++ socket_server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -11,4 +12,4 @@ def test_connect(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, port=int(os.environ.get('PORT', 5000)))
