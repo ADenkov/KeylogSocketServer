16f4280b1fecd89eb3350114b7a2a7a5fb4929b1
i444076 <a.denkov@student.fontys.nl>
2021-06-16 13:30:55 +0200

    Test

--- static/scripts/socketio.js
+++ static/scripts/socketio.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     
     var socket = io();
-    
     console.log(socket)
 
     socket.on('connect', function () {
@@ -14,28 +13,21 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log(`Message received: ${data}`);
     });
 
-    socket.on('disconnect', data => {
-        console.log("Socket disconnected.", data.message);
-        socket.reconnect();
+    socket.on('disconnect', function () {
+        console.log("Socket disconnected.");
+        socket.socket.reconnect();
     });
 
     socket.on('connect_error', (err) => {
         console.log(err.message);
     });
 
-    socket.on('keylogger', data => {
-        console.log("Right spot: ", data);
-
-        var list = document.getElementById('keylogger');
-        var entry = document.createElement('li');
-        var text = data.replace(/'(.*?)'/g, '<span class="red">$1</span>');
-        $("#keylogger").append('<li>'+text+'</li>');
+    socket.on('shit', data => {
+        console.log(data);
     });
 
-
-    // socket.on('keylogger', function (data) {
-    //     console.log(data);
-    //     $("#keylogs").append('<li>' + data + '</li>');
-    // });
+    socket.on('keylogger', function (data) {
+        $("#keylogs").append('<li>' + data + '</li>');
+    });
 
 })
